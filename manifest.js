94177ffa3c1cb627d56a5628529536d66dfc544f
Natanaeldsan <78664393+Natanaeldsan@users.noreply.github.com>
2024-09-03 20:07:43 -0300

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,255 @@
+const canvas = document.getElementById('reservoirCanvas');
+const ctx = canvas.getContext('2d');
+const cdControl = document.getElementById('cd');
+const radiusControl = document.getElementById('radius');
+const heightControl = document.getElementById('height');
+const cdSlider = document.getElementById('cdSlider');
+const radiusSlider = document.getElementById('radiusSlider');
+const heightSlider = document.getElementById('heightSlider');
+const cdValueLabel = document.getElementById('cdValue');
+const radiusValueLabel = document.getElementById('radiusValue');
+const heightValueLabel = document.getElementById('heightValue');
+const timeLabel = document.getElementById('timeLabel');
+const flowRateLabel = document.getElementById('flowRateLabel');
+const heightText = document.getElementById('heightText');
+const pauseButton = document.getElementById('pauseButton');
+const heightOrificioLabel= document.getElementById('heightOrificioLabel')
+const heightOrificioLabelslider= document.getElementById('heightOrificioLabelslider')
+
+
+const storedData = localStorage.getItem('dadosReservatorio');
+const data = JSON.parse(storedData);
+const gravity = data.gravidade;
+const maxDepth = data.profundidade;
+heightControl.max = maxDepth;
+heightControl.value = maxDepth;
+heightSlider.max = maxDepth;
+const orificeType= dadosReservatorio.tipoOrificio;
+
+
+const timeStep = 0.1; // Passo de tempo (s)
+let fluidHeight = canvas.height; // Altura inicial do fluido
+let totalTime = 0; // Tempo total para esvaziar
+let isPaused = true; // Estado da simulação (inicialmente pausado)
+
+function adjustCanvasSize() {
+    canvas.width = document.querySelector('.container').offsetWidth * 0.6; // Ajusta a largura do canvas
+    canvas.height = 400; // Altura fixa para o canvas
+    fluidHeight = canvas.height; // Redefine a altura do fluido
+    drawReservoir(); // Redesenha o reservatório
+}
+
+function calculateFlowRateAndTime() {
+    const Cd = parseFloat(cdControl.value);
+    const radius = parseFloat(radiusControl.value) / 100; // Convertendo para metros
+    const outletArea = Math.PI * Math.pow(radius, 2);
+    const initialHeight = parseFloat(heightControl.value);
+
+    // Cálculo da vazão
+    const flowRate = Cd * outletArea * Math.sqrt(2 * gravity * initialHeight);
+    
+    // Cálculo do tempo total para esvaziar o reservatório
+    let time = 0;
+    let height = initialHeight;
+
+    while (height > 0) {
+        const deltaHeight = (flowRate * timeStep) / (canvas.width / initialHeight); // Corrigido
+        height -= deltaHeight;
+
+        if (height < 0) height = 0;
+
+        time += timeStep;
+    }
+
+    return { flowRate, time };
+}
+
+function drawReservoir() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Desenhar o reservatório
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Desenhar o fluido
+    ctx.fillStyle = '#007BFF';
+    ctx.fillRect(0, canvas.height - fluidHeight, canvas.width, fluidHeight);
+    
+    
+    const radiusLabel = document.getElementById('radiusLabel');
+    const radiusControl = document.getElementById('radius');
+    const radiusSlider = document.getElementById('radiusSlider');
+
+    const widthLabel = document.getElementById('widthLabel');
+    const heightLabel = document.getElementById('heightOrificioLabel');
+    const widthControl = document.getElementById('width');
+    const heightControl = document.getElementById('heightOrificio');
+    const widthSlider = document.getElementById('widthSlider');
+    const heightSlider = document.getElementById('heightOrificioSlider');
+
+
+
+    console.log(orificeType)
+    if (orificeType== "circular"){
+        radiusLabel.style.display = 'block';
+        radiusControl.style.display = 'block';
+        radiusSlider.style.display = 'block';
+
+    }
+    else if(orificeType== "retangular"){
+        widthLabel.style.display = 'block';
+        heightLabel.style.display = 'block';
+        widthControl.style.display = 'block';
+        heightControl.style.display = 'block';
+        widthSlider.style.display = 'block';
+        heightSlider.style.display = 'block';
+    }
+
+        // Acessa os elementos
+        
+
+       
+     
+  
+    
+
+    // Desenhar o orifício
+    const orificeRadius = parseFloat(radiusControl.value) / 2; // Raio do orifício em pixels
+    const orificeX = canvas.width - orificeRadius - 10; // Lado direito do canvas com margem
+    const orificeY = canvas.height - orificeRadius; // Localização do orifício na borda inferior
+
+    ctx.beginPath();
+    ctx.arc(orificeX, orificeY, orificeRadius, 0, 2 * Math.PI);
+    ctx.fillStyle = '#FF0000'; // Cor do orifício
+    ctx.fill();
+    // Remover a linha de contorno
+    ctx.strokeStyle = 'transparent';
+    ctx.stroke();
+    
+    // Desenhar a altura da lâmina d'água
+    ctx.fillStyle = '#FF0000';
+    ctx.font = '16px Arial';
+    ctx.fillText(`Altura: ${(fluidHeight / canvas.height * parseFloat(heightControl.value)).toFixed(3)} m`, 10, 20);
+}
+
+function updateReservoir() {
+    if (isPaused) return;
+
+    const Cd = parseFloat(cdControl.value);
+    const radius = parseFloat(radiusControl.value) / 100;
+    const outletArea = Math.PI * Math.pow(radius, 2);
+    const heightInMeters = fluidHeight / canvas.height * parseFloat(heightControl.value); // altura da lâmina d'água atual
+
+    const flowRate = Cd * outletArea * Math.sqrt(2 * gravity * heightInMeters);
+    const flowRateLiters = flowRate * 1000; // Converte de m³/s para L/s
+
+    // Corrigir o cálculo de deltaHeight
+    const deltaHeight = (flowRate * timeStep * canvas.height) / (canvas.width * parseFloat(heightControl.value));
+
+    fluidHeight -= deltaHeight;
+
+    if (fluidHeight < 0) {
+        fluidHeight = 0;
+    }
+
+    drawReservoir();
+
+    totalTime -= timeStep;
+    timeLabel.textContent = `Tempo de Esvaziamento Restante: ${Math.max(totalTime, 0).toFixed(0)} s`;
+    flowRateLabel.textContent = `Vazão: ${flowRate.toFixed(4)} m³/s (${flowRateLiters.toFixed(2)} L/s)`;
+    heightText.textContent = `Altura da Lâmina d'Água: ${(fluidHeight / canvas.height * parseFloat(heightControl.value)).toFixed(2)} m`;
+
+    if (fluidHeight > 0 && totalTime > 0) {
+        requestAnimationFrame(updateReservoir);
+    }
+}
+
+function resetSimulation() {
+    
+    fluidHeight = canvas.height;
+    const { flowRate, time } = calculateFlowRateAndTime();
+    const flowRateLiters = flowRate * 1000;
+    totalTime = time;
+    drawReservoir();
+
+    // Atualizar labels sem iniciar a simulação
+    timeLabel.textContent = `Tempo de Esvaziamento Estimado: ${totalTime.toFixed(0)} s`;
+    flowRateLabel.textContent = `Vazão Estimada: ${flowRate.toFixed(4)} m³/s (${flowRateLiters.toFixed(2)} L/s)`;
+}
+
+function updateLabels() {
+    cdValueLabel.textContent = cdControl.value;
+    radiusValueLabel.textContent = radiusControl.value;
+    heightValueLabel.textContent = heightControl.value;
+}
+
+function togglePause() {
+    isPaused = !isPaused;
+    pauseButton.textContent = isPaused ? 'Esvaziar' : 'Pausar';
+
+    if (!isPaused) {
+        updateReservoir();
+    }
+}
+
+function enforceLimits() {
+    let cdValue = parseFloat(cdControl.value);
+    let radiusValue = parseFloat(radiusControl.value);
+    let heightValue = parseFloat(heightControl.value);
+
+    if (cdValue < 0.3) cdControl.value = 0.3;
+    if (cdValue > 0.99) cdControl.value = 0.99;
+    if (radiusValue < 1) radiusControl.value = 1;
+    if (radiusValue > 15) radiusControl.value = 15;
+    if (heightValue < 0.01) heightControl.value = 0.01;
+    if (heightValue > 20) heightControl.value = 20;
+    
+    updateLabels();
+}
+
+function syncSliders() {
+    cdSlider.value = cdControl.value;
+    radiusSlider.value = radiusControl.value;
+    heightSlider.value = heightControl.value;
+}
+
+cdControl.addEventListener('input', () => {
+    enforceLimits();
+    updateLabels();
+    syncSliders();
+    resetSimulation();
+});
+radiusControl.addEventListener('input', () => {
+    enforceLimits();
+    updateLabels();
+    syncSliders();
+    resetSimulation();
+});
+heightControl.addEventListener('input', () => {
+    enforceLimits();
+    updateLabels();
+    syncSliders();
+    resetSimulation();
+});
+
+cdSlider.addEventListener('input', () => {
+    cdControl.value = cdSlider.value;
+    updateLabels();
+    resetSimulation();
+});
+radiusSlider.addEventListener('input', () => {
+    radiusControl.value = radiusSlider.value;
+    updateLabels();
+    resetSimulation();
+});
+heightSlider.addEventListener('input', () => {
+    heightControl.value = heightSlider.value;
+    updateLabels();
+    resetSimulation();
+});
+
+pauseButton.addEventListener('click', togglePause);
+
+adjustCanvasSize();
+window.addEventListener('resize', adjustCanvasSize); // Redimensiona o canvas quando a janela for redimensionada
+resetSimulation();
